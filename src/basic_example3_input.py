4fe10ec349ee4f798184a90fefdec114b2ef2c9c
romy <romy@snowyla.com>
2024-08-12 15:28:15 -0700

    some samples

--- src/basic_example3_input.py
+++ src/basic_example3_input.py
@@ -5,22 +5,21 @@ from langchain.schema.output_parser import StrOutputParser
 
 
 ## this is a basic example that comes from the documentation for Langchain- Ollama
-### https://python.langchain.com/v0.2/docs/integrations/llms/ollama/
-template = """Question: {question}
-Answer: Let's think step by step.
-"""
+### https://python.langchain.com/v0.2/docs/integrations/llms/ollama/ the open-source AI a question and have it explain step by step\n")
 
-user_input = input("Ask the open-source AI a question and have it explain step by step\n")
+user_input = input("Give a topic that the AI will explain to you in steps\n")
 application_prompt = """Given the following topic and then explain what it is step by step
     {user_input}
 """
 
-prompt = PromptTemplate(input_variables=["user_input"], template=application_prompt)
+prompt = PromptTemplate(input_variables=["user_input"], 
+                        template=application_prompt)
 
-llm = OllamaLLM(model="llama3.1:latest", temperature=0.7, max_tokens=500)
+llm = OllamaLLM(model="llama3.1:latest", 
+                temperature=1, 
+                max_tokens=1000)
 
 chain = prompt | llm | StrOutputParser()
-
 
 result = chain.invoke({"user_input": {user_input}})
 
