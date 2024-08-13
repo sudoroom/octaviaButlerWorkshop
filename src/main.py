d5b6f0554869bcfd9b356af0dbe4d0358fb37b1a
romy <romy@snowyla.com>
2024-08-12 17:36:26 -0700

    added working

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
-from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from starlette.responses import FileResponse
 from services.ai import generate_response
 
 app = FastAPI()
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+@app.get("/")
 async def read_index():
     return FileResponse('static/index.html')
 
 @app.get("/api/gpt")
 async def gpt(review: str):
     return generate_response(review)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
