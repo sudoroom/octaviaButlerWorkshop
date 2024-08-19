801bbdda11cca3372a5a0a732018ad2c82ce83f3
romy <romy@snowyla.com>
2024-08-19 16:06:50 -0700

    got it up and running on llama index

--- src/simple_rag_llama_index.py
+++ src/simple_rag_llama_index.py
@@ -1,10 +1,19 @@
 from llama_index.core import Document, VectorStoreIndex
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.core import Settings
+from llama_index.llms.ollama import Ollama
 
 #import logging
 #import sys
 #logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 #logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
+llm = Ollama(model="llama3.1:latest", request_timeout=120.0)
+
+Settings.embed_model = HuggingFaceEmbedding(
+    model_name="BAAI/bge-small-en-v1.5"
+)
+Settings.llm = llm
 documents = [
     Document(text="Abraham Lincoln was the 16th president of the United States."),
     Document(text="Abraham Shakespeare was a Florida lottery winner in 2006."),
