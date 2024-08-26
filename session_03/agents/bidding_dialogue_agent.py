b5d6920f71076c134bf4dc901b580927181520e3
romy <romy@snowyla.com>
2024-08-26 16:25:15 -0700

    draft one redo it

--- session_03/agents/bidding_dialogue_agent.py
+++ session_03/agents/bidding_dialogue_agent.py
@@ -1,26 +1,14 @@
 from agents.dialogue_agent import DialogueAgent
 
 class BiddingDialogueAgent(DialogueAgent):
-    def __init__(
-        self,
-        name,
-        system_message: SystemMessage,
-        bidding_template: PromptTemplate,
-        model: ChatOpenAI,
-    ) -> None:
-        super().__init__(name, system_message, model)
+    def __init__(self, name: str, system_message: str, bidding_template: str) -> None:
+        super().__init__(name, system_message)
         self.bidding_template = bidding_template
 
     def bid(self) -> str:
-        """
-        Asks the chat model to output a bid to speak
-        """
-        prompt = PromptTemplate(
-            input_variables=["message_history", "recent_message"],
-            template=self.bidding_template,
-        ).format(
+        prompt = self.bidding_template.format(
             message_history="\n".join(self.message_history),
             recent_message=self.message_history[-1],
         )
-        bid_string = self.model([SystemMessage(content=prompt)]).content
+        bid_string = self.ollama_call(prompt)
         return bid_string
